7635ca45dfd64da39f86d476849d4b1e5e21f42c
Darshil122 <darshildabhi2005@gmail.com>
2025-05-17 08:45:29 +0530

    Update file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+// import About from './components/About';
+import  Navbar  from "./components/Navbar";
+import TextForm from './components/TextForm';
+import { useState } from "react";
 
 function App() {
+  const [mode, setMode] = useState("light");
+  
+  const togglemode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');  
+      document.body.style.backgroundColor = "gray";
+      document.body.style.color = "white"; 
+    }else{
+      setMode('light');
+      document.body.style.backgroundColor = "white";
+      document.body.style.color = "black";
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+<Navbar title = "TextUtils" mode={mode} togglemode={togglemode}/>
+<div className='container my-3'>
+<TextForm heading = "Enter the text analyze below" mode={mode}/>
+  {/* <About /> */}
+</div>
+    </>
   );
 }
 
